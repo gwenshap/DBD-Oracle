666491d50c6c8611700807ccdbde0ad4e535155b
byterock <byterock@50811bd7-b8ce-0310-adc1-d9db26280581>
2010-07-21 01:22:47 +0000

    Fix for invalid format in trace of OCILobLocatorIsInit_log_stat reported by Martin Evans Fixed by John Scoles yet again
    git-svn-id: http://svn.perl.org/modules/dbd-oracle/trunk@14263 50811bd7-b8ce-0310-adc1-d9db26280581

--- ocitrace.h
+++ ocitrace.h
@@ -119,12 +119,12 @@
 				 OciTp,	(void*)svchp,(void*)envhp, src_type, src_ptr,oci_status_name(stat)),stat \
 	: stat
 
-#define OCILobLocatorIsInit_log_stat(envhp,errhp,loc,is_init,stat)\
+#define OCILobLocatorIsInit_log_stat(envhp,errhp,loc,is_initp,stat)\
 	stat =OCILobLocatorIsInit (envhp,errhp,loc,is_init );\
 	(DBD_OCI_TRACEON) \
 			? PerlIO_printf(DBD_OCI_TRACEFP,\
 				 "%sOCILobLocatorIsInit_log_stat(%p,%p,%p,%d)=%s\n",\
-				 OciTp, (void*)envhp, (void*)errhp,loc,is_init,oci_status_name(stat)),stat \
+				 OciTp, (void*)envhp, (void*)errhp,loc,*is_initp,oci_status_name(stat)),stat \
 	: stat
 
 #define OCIObjectPin_log_stat(envhp,errhp,or,ot,stat)\
