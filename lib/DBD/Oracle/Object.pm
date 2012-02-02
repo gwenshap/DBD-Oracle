ae853defe36aa06a3de13c4bf69bbaab36409f44
Yanick Champoux <yanick@babyl.dyndns.org>
2012-02-02 16:32:49 -0500

    Build results of 10047cf (on master)

--- lib/DBD/Oracle/Object.pm
+++ lib/DBD/Oracle/Object.pm
@@ -1,4 +1,10 @@
 package DBD::Oracle::Object;
+{
+  $DBD::Oracle::Object::VERSION = '1.38';
+}
+BEGIN {
+  $DBD::Oracle::Object::AUTHORITY = 'cpan:PYTHIAN';
+}
 # ABSTRACT: Wrapper for Oracle objects
 
 use strict;
@@ -23,3 +29,42 @@ sub attr {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+DBD::Oracle::Object - Wrapper for Oracle objects
+
+=head1 VERSION
+
+version 1.38
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Tim Bunce <timb@cpan.org>
+
+=item *
+
+John Scoles
+
+=item *
+
+Yanick Champoux <yanick@cpan.org>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 1994 by Tim Bunce.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
