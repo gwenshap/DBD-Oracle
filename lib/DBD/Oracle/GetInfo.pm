033eaebed076280b6b205e31dc2ddd9ed12ddfdb
Yanick Champoux <yanick@babyl.dyndns.org>
2012-03-30 14:36:18 -0400

    Build results of 761e712 (on master)

--- lib/DBD/Oracle/GetInfo.pm
+++ lib/DBD/Oracle/GetInfo.pm
@@ -1,4 +1,10 @@
 package DBD::Oracle::GetInfo;
+{
+  $DBD::Oracle::GetInfo::VERSION = '1.43_00';
+}
+BEGIN {
+  $DBD::Oracle::GetInfo::AUTHORITY = 'cpan:PYTHIAN';
+}
 # ABSTRACT: Wrapper to get Oracle information
 
 use strict;
@@ -278,5 +284,45 @@ our %info = (
 );
 
 1;
+
+
+
+=pod
+
+=head1 NAME
+
+DBD::Oracle::GetInfo - Wrapper to get Oracle information
+
+=head1 VERSION
+
+version 1.43_00
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Tim Bunce <timb@cpan.org>
+
+=item *
+
+John Scoles
+
+=item *
+
+Yanick Champoux <yanick@cpan.org>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 1994 by Tim Bunce.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 
 __END__
