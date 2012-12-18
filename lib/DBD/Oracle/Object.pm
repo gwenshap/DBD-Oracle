fdd4d20d045d7400ef73ae9fffc332ba997cc5c1
yanick <yanick@50811bd7-b8ce-0310-adc1-d9db26280581>
2012-12-18 16:06:45 +0000

    Preps for release 1.53_00
    git-svn-id: http://svn.perl.org/modules/dbd-oracle/trunk@15521 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/DBD/Oracle/Object.pm
+++ lib/DBD/Oracle/Object.pm
@@ -1,6 +1,6 @@
 package DBD::Oracle::Object;
 {
-  $DBD::Oracle::Object::VERSION = '1.52';
+  $DBD::Oracle::Object::VERSION = '1.53_00';
 }
 BEGIN {
   $DBD::Oracle::Object::AUTHORITY = 'cpan:PYTHIAN';
@@ -40,7 +40,7 @@ DBD::Oracle::Object - Wrapper for Oracle objects
 
 =head1 VERSION
 
-version 1.52
+version 1.53_00
 
 =head1 AUTHORS
 
