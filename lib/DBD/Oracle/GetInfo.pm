0dbbeaedd26d391b7fb6dc03378864e320141cfd
yanick <yanick@50811bd7-b8ce-0310-adc1-d9db26280581>
2013-02-07 22:11:28 +0000

    version bump to v1.57_00
    git-svn-id: http://svn.perl.org/modules/dbd-oracle/trunk@15570 50811bd7-b8ce-0310-adc1-d9db26280581

--- lib/DBD/Oracle/GetInfo.pm
+++ lib/DBD/Oracle/GetInfo.pm
@@ -1,6 +1,6 @@
 package DBD::Oracle::GetInfo;
 {
-  $DBD::Oracle::GetInfo::VERSION = '1.56';
+  $DBD::Oracle::GetInfo::VERSION = '1.57_00';
 }
 BEGIN {
   $DBD::Oracle::GetInfo::AUTHORITY = 'cpan:PYTHIAN';
@@ -295,7 +295,7 @@ DBD::Oracle::GetInfo - Wrapper to get Oracle information
 
 =head1 VERSION
 
-version 1.56
+version 1.57_00
 
 =head1 AUTHORS
 
